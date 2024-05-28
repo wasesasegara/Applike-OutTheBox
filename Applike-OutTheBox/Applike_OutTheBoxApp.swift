fe326e317d8ea54adb0eb63cd67129154f71190b
Bis <wasesasegara@gmail.com>
2024-05-28 10:53:10 +0700

    Init Project

---
+++ Applike-OutTheBox/Applike_OutTheBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Applike_OutTheBoxApp.swift
+//  Applike-OutTheBox
+//
+//  Created by Bis  on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Applike_OutTheBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
